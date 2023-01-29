3577b2e15e623b9343698e758b9c08573f43fb3f
AKSHAY <akshayatwwe@gmail.com>
2023-01-30 01:27:36 +0530

    Add project files.

---
+++ Controllers/UserDetailsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MP1.Controllers
+{
+    public class UserDetailsController : Controller
+    {
+        // GET: UserDetailsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UserDetailsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserDetailsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserDetailsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserDetailsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserDetailsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserDetailsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserDetailsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/UserDetails.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+namespace MP1.Models
+{
+    public class UserDetails
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public string EmailId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+
+        public static void Insert(UserDetails u)
+        {
+            SqlConnection cn=new SqlConnection();
+        }
+    }
+}
